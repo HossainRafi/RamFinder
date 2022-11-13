5b1df606ce03f12b38e35e86ae781494e0d7ca47
HossainRafi <hossainrafi71@gmail.com>
2022-11-13 20:23:12 +0600

    update total

--- script.js
+++ script.js
@@ -32,4 +32,21 @@ function ready() {
 function removeCartItem(event) {
   let buttonClicked = event.target;
   buttonClicked.parentElement.remove();
+  updateTotal();
+}
+
+// update total
+function updateTotal() {
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  let cartBoxes = cartContent.getElementsByClassName("cart-box");
+  let total = 0;
+  for (let i = 0; i < cartBoxes.length; i++) {
+    const cartBox = cartBoxes[i];
+    let priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    let price = parseFloat(priceElement.innerText.replace("$", ""));
+    let quantity = quantityElement.value;
+    total = total + price * quantity;
+    document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+  }
 }
