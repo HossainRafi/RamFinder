45fbc0d8eaf18783426cb6b9a38a6041169c86ef
HossainRafi <hossainrafi71@gmail.com>
2022-11-13 20:02:29 +0600

    remove items from cart

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ let closeCart = document.querySelector("#close-cart");
 
 // open cart
 cartIcon.onclick = () => {
-    cart.classList.add("active")
-}
+  cart.classList.add("active");
+};
 
 // close cart
 closeCart.onclick = () => {
-    cart.classList.remove("active")
+  cart.classList.remove("active");
+};
+
+// making cart functional
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// remove items from cart
+function ready() {
+  let removeCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButtons);
+  for (let i = 0; i < removeCartButtons.length; i++) {
+    const button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+}
+function removeCartItem(event) {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
 }
